cd0aae703d5e855ad9fcc88ca0615aaa04f8c98b
loveleen2605 <loveleen40454@gmail.com>
2023-11-24 10:47:45 -0500

    first commit

--- Program.fs
+++ Program.fs
@@ -41,38 +41,35 @@ let successPercentages =
 
 
 type Cuisine = 
-| Korean
-| Turkish
+    | Korean
+    | Turkish
 
 type MovieType =
-| Regular
-| IMAX
-| DBOX
-| RegularWithSnacks
-| IMAXWithSnacks
-| DBOXWithSnacks
+    | Regular
+    | IMAX
+    | DBOX
+    | RegularWithSnacks
+    | IMAXWithSnacks
+    | DBOXWithSnacks
 
 type Activity =
-| BoardGame
-| Chill
-| Movie of MovieType
-| Restaurant of Cuisine
-| LongDrive of int * float
-
+    | BoardGame
+    | Chill
+    | Movie of MovieType
+    | Restaurant of Cuisine
+    | LongDrive of int * float
 
 let calculateBudget activity =
-   match activity with 
-   | BoardGame | Chill -> 0.0
-   | Movie Regular -> 12.0
-   | Movie IMAX -> 17.0
-   | Movie DBOX -> 20.0
-   | Movie RegularWithSnacks | Movie IMAXWithSnacks | Movie DBOXWithSnacks -> 12.0 + 5.0 
-   | Restaurant Korean -> 70.0
-   | Restaurant Turkish -> 65.0
-   | LongDrive (kilometres, fuelcharge) -> float kilometres * fuelcharge
+    match activity with 
+    | BoardGame | Chill -> 0.0
+    | Movie Regular -> 12.0
+    | Movie IMAX -> 17.0
+    | Movie DBOX -> 20.0
+    | Movie RegularWithSnacks | Movie IMAXWithSnacks | Movie DBOXWithSnacks -> 12.0 + 5.0 
+    | Restaurant Korean -> 70.0
+    | Restaurant Turkish -> 65.0
+    | LongDrive (kilometres, fuelcharge) -> float kilometres * fuelcharge
 
 let eveningActivity = Restaurant Turkish
 let budget = calculateBudget eveningActivity
 printfn "Estimated Budget: %.2f CAD" budget
-
-
