4ba4b78a7378ac2f3e9e6fd4dc80c8556945df67
loveleen2605 <loveleen40454@gmail.com>
2023-11-24 09:55:11 -0500

    first commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿type Coach = {
+    Name: string
+    FormerPlayer: bool
+}
+
+type Stats = {
+    Wins: int
+    Losses: int
+}
+
+type Team = {
+    Name: string
+    Coach: Coach
+    Stats: Stats
+}
